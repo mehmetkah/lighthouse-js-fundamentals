81368f2ca46a1e40d749eed1f4907325c07dd34a
Fatih Kahraman <mehmetkahca@gmail.com>
2022-04-30 01:41:40 -0600

    new

--- conditionals.js
+++ conditionals.js
@@ -1,18 +1,9 @@
-const temperature = 20;
-const raining = true;
-
-if (temperature < 0) {
-  console.log("Make sure you pick out a scarf!");
-} else if (temperature < 15) {
-  console.log("Short sleeves won't cut it!");
-} else {
-  console.log("Short sleeves are fine.");
+const whichSchool = function (age) {
+    if (age < 13) {
+        return 'Elementary School'
+    } else if (age >= 13 && age <= 18) {
+        return "Secondary School"
+    } else {
+        return 'Lighthouse Labs'
+    }
 }
-
-console.log("Now you're ready to go outside!");
-if (temperature < -40 || temperature > 40) {
-    console.log("Maybe going outside isn't such a great idea…");
-  }
-  if (!raining) {
-    console.log("Leave your umbrella at home!");
-  }
